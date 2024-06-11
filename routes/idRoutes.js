fe8a33eba0f3deb1cfa496629ff68619ab80baf1
bslowik4 <bslowik4@outlook.com>
2024-06-11 19:18:21 +0200

    Added multer package
    Added package which allows to receive photos

--- routes/idRoutes.js
+++ routes/idRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
+const imageController = require('../controllers/imageController');
 
 router.get('/:id', clientController.getClient);
+
+router.post('/', imageController.getImage);
 
 module.exports = router;
