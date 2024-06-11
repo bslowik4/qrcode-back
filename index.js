0cdabf16532895c91f00794752c1edb406060e05
bslowik4 <bslowik4@outlook.com>
2024-06-11 18:46:35 +0200

    Created basic directory layout
    Added directories necessary for further work

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+const idRoutes = require('./routes/idRoutes');
+app.use('/edit', idRoutes);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
