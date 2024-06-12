d656c148733fdbbf0bb9dcbbe746b6884861ec07
bslowik4 <bslowik4@outlook.com>
2024-06-12 10:36:09 +0200

    Creating video
    Added file system and dummy data

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,4 +1,12 @@
+const createVideo = require('../utils/createVideo');
+
 exports.getClient = (req, res) => {
     const clientId = req.params.id
     res.send(`Details of user with ID: ${clientId}`)
+}
+
+exports.renderVideo = (req, res) => {
+    const clientId = req.params.id
+    res.send(`Details of user with ID: ${clientId}`)
+    createVideo(['../uploads/test1.png','../uploads/test2.png'])
 }
