7e4000067dcadbbab28abf097663b5d0c270691c
bslowik4 <bslowik4@outlook.com>
2024-06-19 18:54:04 +0200

    fixed video creation
    It's now unique for every users. Might look into changing folder from upload and video to one user folder in future

--- utils/createVideo.js
+++ utils/createVideo.js
@@ -1,19 +1,28 @@
-const videoshow = require('videoshow')
+const videoshow = require('videoshow');
+const fs = require('fs');
+const path = require('path');
 
 const createVideo = (images, id) => {
+  const outputDirectory = path.join(__dirname, '..', 'videos', id.toString());
+  const outputVideoPath = path.join(outputDirectory, 'video.mp4');
+  
+  if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory, { recursive: true });
+  }
+
   videoshow(images)
-    .save(`/videos/${id}/video.mp4`)
+    .save(outputVideoPath)
     .on('start', function (command) {
-      console.log('ffmpeg process started:', command)
+      console.log('ffmpeg process started:', command);
     })
     .on('error', function (err) {
-      console.error('Error:', err)
+      console.error('Error:', err);
     })
     .on('end', function (output) {
-      console.log('Video created in:', output)
-    })
-}
+      console.log('Video created in:', output);
+    });
+};
 
 module.exports = {
-    createVideo
-}
+  createVideo
+};
