5d343db45c55e624722c7375defe13684865d6ab
bslowik4 <bslowik4@outlook.com>
2024-06-24 16:47:43 +0200

    Added error handeling
    Created error handeling, checking if file exists

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,18 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+
 const getVideo = (req, res) => {
-  const videoPath = path.join(__dirname, '../videos', req.params.id, req.params.id + '.mp4');
-  const stat = fs.statSync(videoPath);
-  const fileSize = stat.size;
+  const videoPath = path.join(__dirname, '..', 'videos', req.params.id, 'video.mp4');
+  fs.stat(videoPath, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).send('Video file not found');
+      } else {
+        console.error('Error accessing video file:', err);
+        return res.status(500).send('Internal server error');
+      }
+    }
 
-  const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'video/mp4',
-  };
+    const fileSize = stats.size;
 
-  res.writeHead(200, head);
-  fs.createReadStream(videoPath).pipe(res);
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  });
 };
 
 module.exports = { getVideo };
+
