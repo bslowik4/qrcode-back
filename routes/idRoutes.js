59ca4c34571290562acfdb533a35367dfd6a7f59
bslowik4 <bslowik4@outlook.com>
2024-06-26 10:49:57 +0200

    Added authentication routes
    Added authenticetion routes to secure paths

--- routes/idRoutes.js
+++ routes/idRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const clientController = require('../controllers/clientController');
 const imageController = require('../controllers/imageController');
 const upload = require('../middleware/imageUploadMiddleware.js');
+const authMiddleware = require('../middleware/authMiddleware');
 
 
 router.get('/:id', clientController.getClient);
 
-router.post('/:id/create', clientController.renderVideo);
+router.post('/:id/create', authMiddleware, clientController.renderVideo);
 
-router.post('/:id/image', upload.array('processed_photo', 2), imageController.uploadProcessedPhotos);
+router.post('/:id/image', authMiddleware, upload.array('processed_photo', 2), imageController.uploadProcessedPhotos);
 
 module.exports = router;
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getVideo } = require('../controllers/videoController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.get('/:id', getVideo);
+router.get('/:id', authMiddleware, getVideo);
 
 module.exports = router;
